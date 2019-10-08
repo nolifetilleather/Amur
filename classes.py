3dd1bd68bb27cebee46dd5b1c2a6dd22a161d9f1
WhoopeeyGoldberg <rehtael.lit.efil.on@gmail.com>
2019-10-08 09:58:59 +0300

    SignalsList methods was named

--- classes.py
+++ classes.py
@@ -6,7 +6,7 @@ class Signal:
             self,
             name,
             plc,
-            signal_type=None,
+            sigtype=None,
             position=None,
             location=None,
             ff_out=None,
@@ -18,7 +18,7 @@ class Signal:
             self.name = 'P' + self.name
 
         self.plc = plc
-        self.signal_type = signal_type
+        self.sigtype = sigtype
         self.position = position
         self.location = location
         self.ff_out = ff_out
@@ -26,4 +26,47 @@ class Signal:
         self.address = address
 
 
+class SignalsList(list):
 
+    @staticmethod
+    def __any_signal_sigtype_in(signal, sigtypes_list):
+        pass
+
+    # $$$$$$$$$$$$$$ ПРОВЕРКА НАЛИЧИЯ СИГНАЛОВ $$$$$$$$$$$$$$$$$$$
+    # $$$$$$$$$$$$$$            ДЛЯ            $$$$$$$$$$$$$$$$$$$
+    # $$$$$$$$$$$$$$   INPUT, OUTPUT, ALARMING $$$$$$$$$$$$$$$$$$$
+
+    def contains_signals_for_input_txt(self):
+        pass
+
+    def contains_signals_for_output_txt(self):
+        pass
+
+    def contains_signals_for_alarming_txt(self):
+        pass
+
+    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    # $$$$$$$$$$ ПРОВЕРКА НАЛИЧИЯ СИГНАЛОВ ДЛЯ COUNTING $$$$$$$$$$
+    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+    def contains_signals_for_counting_txt(self):
+        pass
+
+    def contains_signals_with_ff_out_for_counting_txt(self):
+        pass
+
+    def contains_signals_without_ff_out_for_counting_txt(self):
+        pass
+
+    def contains_signals_with_fire_fighting_for_counting_txt(self):
+        pass
+
+    def contains_signals_with_fire_for_counting_counting_txt(self):
+        pass
+
+    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    # $$$$$$$$$$$ ПРОВЕРКА НАЛИЧИЯ СИГНАЛОВ ДЛЯ DIAG_ST $$$$$$$$$$
+    # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+    def contains_signals_for_diag_st_txt(self):
+        pass
